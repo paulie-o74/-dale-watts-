c4c82388b5bf56b1d5a7484a861d4c5fe31be19b
rashdogg74 <pauloriordan74@gmail.com>
2021-12-06 19:53:08 +0000

    Complete Javascript for end page which will send user home after submitting highscore

--- assets/js/end.js
+++ assets/js/end.js
@@ -3,7 +3,7 @@ const saveScoreButton = document.getElementById("saveScoreButton");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup" , () => {
@@ -15,11 +15,12 @@ saveHighScore = e => {
     e.preventDefault();
 
     const score = {
-        score: mostRecentScore,
+        score: Math.floor(Math.random() * 100),
         name: username.value
     }
     highScores.push(score);
-
     highScores.sort( (a,b) => b.score - a.score)
-    console.log(highScores);
+    highScores.splice(5);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/")
 };
