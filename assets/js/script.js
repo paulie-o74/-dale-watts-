e6fbba83283dfed60080ffbc1caeb3bf9148e576
rashdogg74 <pauloriordan74@gmail.com>
2021-12-06 11:32:29 +0000

    Add javascript for displaying feedback for correct / incorrect answers and connect JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-const question = document.getElementById("question");
-const question = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -14,7 +14,7 @@ let questions = [
         choice2: "<js>",
         choice3: "<java>",
         choice4: "<scripting>",
-        answer: 1
+        answer: 1,
     },
     {
         question: "Inside which HTML element do we put the css?",
@@ -22,7 +22,7 @@ let questions = [
         choice2: "<ss>",
         choice3: "<css>",
         choice4: "<styles>",
-        answer: 3
+        answer: 3,
     },
     {
         question: "Inside which HTML element do we put the html?",
@@ -30,29 +30,60 @@ let questions = [
         choice2: "<t>",
         choice3: "<m>",
         choice4: "<html>",
-        answer: 4
+        answer: 4,
     },
 ]
 
 // constants
 
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    console.log(availableQuestions;)
     getNewQuestion();
 };
 
 getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // go to the end page
+        return window.location.assign("end.html");
+    }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
 };
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+        selectedChoice.classList.add(classToApply);
+
+        setTimeout( () => {
+            selectedChoice.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    })
+})
 
 startGame();
 
