1fe55e67cd8d400b56a1b124446cdd87afe7243d
rashdogg74 <pauloriordan74@gmail.com>
2021-12-09 21:01:55 +0000

    Add commenting to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
+// DOM queering 
 const question = document.getElementById('question');
+// We want to return an array so that we can use it... specifically the dataset property which will help determine the correct answers
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const questionCounterText = document.getElementById('questionCounter');
 const scoreText = document.getElementById('score');
 const loader = document.getElementById('loader');
 const game = document.getElementById('game');
-let currentQuestion = {};
-let acceptingAnswers = false;
+// Variables below
+let currentQuestion = {}; // this is an object
+let acceptingAnswers = false;// we don't want to allow users to answer until everything is loaded hence the false
 let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+let questionCounter = 0; //What question are you on
+let availableQuestions = []; //empty array for hard coded questions so that we don't give the same questions twice
 
-let questions = [];
+let questions = []; // an array remove the hard coded questions afterwards 
 
 fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=multiple')
     .then((res) => {
@@ -39,45 +42,47 @@ fetch('https://opentdb.com/api.php?amount=10&category=12&difficulty=medium&type=
 // });
 
 // constants
-
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 10;
+const MAX_QUESTIONS = 10; //How many questions in the game
+
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+    availableQuestions = [...questions]; // The three dots means take the question array and spread out all of its items and 
+    //put them into a new array we want a full copy so that when changes are made they are reflected in this 
+    getNewQuestion(); 
     game.classList.remove("hide");
     loader.classList.add("hide");
 };
 
+// function to get new questions
 getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score);
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) { // || means or
+        localStorage.setItem('mostRecentScore', score); //store the users score
         // go to the end page
         return window.location.assign("end.html");
     }
-    questionCounter++;
+    questionCounter++; //This means that the counter will increment each question
     questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length); // To get random questions so that we can randomise the order of questions
     currentQuestion = availableQuestions[questionIndex];
-    question.innerHTML = currentQuestion.question;
+    question.innerHTML = currentQuestion.question; //Sets the question or populates it
 
     choices.forEach(choice => {
         const number = choice.dataset["number"];
-        choice.innerHTML = currentQuestion["choice" + number];
+        choice.innerHTML = currentQuestion["choice" + number]; // Getting the choices plus the dataset number property
     });
 
-    availableQuestions.splice(questionIndex, 1);
+    availableQuestions.splice(questionIndex, 1); //Takes the available questions array and splices out the one we just used
 
-    acceptingAnswers = true;
+    acceptingAnswers = true; //This will allow users to answer because the questions are now loaded
 };
-
+//We want to add event listeners so we know a choice has been chosen
 choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if (!acceptingAnswers) return;
+    choice.addEventListener("click", e => { 
+        if (!acceptingAnswers) return; //if we're not ready then return (ignore) the fact they clicked
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
@@ -93,7 +98,7 @@ choices.forEach(choice => {
 
         setTimeout(() => {
             selectedChoice.classList.remove(classToApply);
-            getNewQuestion();
+            getNewQuestion(); //Loads a new question
         }, 1000);
     })
 })
