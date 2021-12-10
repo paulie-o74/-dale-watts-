1e8dd058cfec5f63ae49f15b1b410a7d1f69e573
rashdogg74 <pauloriordan74@gmail.com>
2021-12-10 19:21:27 +0000

    Add comments to js

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -1,13 +1,18 @@
+// via emailjs
+
+//DOM queering 
 const quizContainer = document.getElementById('form');
 const container2 = document.getElementById('success');
 
 
 function sendMail(params) {
     var tempParams = {
+        // paramaters from the DOM
         from_name: document.getElementById("fromName").value,
         to_name: document.getElementById("email_address").value,
         message: document.getElementById("msg").value,
     };
+    // service ID, template ID, and params object from above
     emailjs.send('service_zgolv4m', 'template_54vj3xp', tempParams)
         .then(function (res) {
             console.log("success", res.status);
@@ -15,7 +20,7 @@ function sendMail(params) {
 };
 
 
-
+//show success message after submitting form via emailJS
 var a;
 
 function successMessage() {
